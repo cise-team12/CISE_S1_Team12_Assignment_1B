301c57ab7d5c5360770e2fab2178649cb9eeaa64
vkj8189 <53281673+vkj8189@users.noreply.github.com>
2020-05-20 16:08:32 +1200

    Update search.php
    Added a query for start date and end date
    - not working yet

--- search.php
+++ search.php
@@ -6,35 +6,32 @@ $database="seer";
 $host="localhost";
 
 $connection=mysqli_connect($host,$username,$password,$database);
+
 if(!$connection){
 	echo "connection failure";
-}
-else{
+}else{
+	if(isset($_POST["sort"])){
+  		$sortSetting = $_POST["sort"];
+	}else{
+		//This is here incase it comes in useful later, radio buttons default to title anyway so this should never be reached
+  		$sortSetting = "Default";
+	}
 
-if(isset($_POST["sort"]))
-{
-  $sortSetting = $_POST["sort"];
-}
-else
-{
-	//This is here incase it comes in useful later, radio buttons default to title anyway so this should never be reached
-  $sortSetting = "Default";
-}
+	if(isset($_POST["enter"])){
+		$search=$_POST["search"];
+		$startDate = $_POST["startDate"];
+		$endDate = $_POST["endDate"];
 
-if(isset($_POST["enter"]))
-		{
-			$search=$_POST["search"];
+		$sqlString = "SELECT * FROM `article` 
+						WHERE title like '%$search%' 
+						AND year BETWEEN $startDate and $endDate 
+						ORDER BY $sortSetting";
 
-			$sqlString = "SELECT * FROM `article` WHERE title like '%$search%' ORDER BY $sortSetting";
+		$sqlResult = mysqli_query($connection, $sqlString);
 
-			$sqlResult=mysqli_query($connection, $sqlString);
-
-			if(!$sqlResult)
-			{
-				echo "<p>Something is wrong with ",	$sqlString , "</p>";
-			}
-			else {
-
+		if(!$sqlResult){
+			echo "<p>Something is wrong with ",	$sqlString , "</p>";
+		}else{
 			echo "<table border=\"1\">";
 			echo "<tr>\n"
 			."<th scope=\"col\">Article ID</th>\n"
@@ -44,8 +41,7 @@ if(isset($_POST["enter"]))
 			."<th scope=\"col\">Year</th>\n"
 			."</tr>\n";
 
-			while ($row = mysqli_fetch_assoc($sqlResult))
-			{
+			while ($row = mysqli_fetch_assoc($sqlResult)){
 				echo "<tr>";
 				echo "<td>",$row["article_id"],"</td>";
 				echo "<td>",$row["title"],"</td>";
@@ -54,14 +50,9 @@ if(isset($_POST["enter"]))
 				echo "<td>",$row["year"],"</td>";
 				echo "</tr>";
 			}
-				echo "</table>";
+			echo "</table>";
 			mysqli_free_result($sqlResult);
 		}
 	}
-
-
-
 }
-
-
 ?>
