c3422970b1e20bd05736da4955f651e030b88730
JoshMallaby <54135723+JoshMallaby@users.noreply.github.com>
2020-06-03 17:51:54 +1200

    Made search.php fully object oriented

--- search.php
+++ search.php
@@ -2,19 +2,74 @@
 <?php
 class DatabaseConnection
 {
-  	public $username="root";
-	public $password="root";
-	public $database="seer";
-	public $host="localhost";
+  public $link;
+  public $dataSet;
 
-  /*public function __construct($i)
+  private $sqlQuery;
+
+  protected $username;
+	protected $password;
+	protected $database;
+	protected $host;
+
+  public function __construct()
   {
-  }*/
+    $this->username="root";
+  	$this->password="root";
+  	$this->database="seer";
+  	$this->host="localhost";
+  }
   public function dbConnect()
   {
-    return mysqli_connect($this->host,$this->username,$this->password,$this->database);
+    $this->link = mysqli_connect($this->host,$this->username,$this->password,$this->database);
+    return $this->link;
+  }
+  public function selectData($link, $search,$yearResult,$startDate,$sortSetting)
+  {
+    $this ->sqlQuery = "SELECT * FROM `article`
+          WHERE (title like '%$search%' OR author like '%$search%' OR method like '%$search%')
+          AND year BETWEEN $yearResult and $startDate
+          ORDER BY $sortSetting";
+          echo $this->sqlQuery;
+    $this ->dataSet = mysqli_query($link, $this ->sqlQuery);
+    return $this ->dataSet;
+  }
+  public function outputData($sqlResult)
+  {
+    if(!$sqlResult)
+    {
+      echo "<p>Something is wrong with ",	$this->sqlQuery , "</p> <br>";
+      //echo "<p>Year Result ",	$yearResult, "</p> <br>";
+    }else
+    {
+      echo "<table class='table table-dark'>";
+      echo "<tr>\n"
+      ."<th scope=\"col\">Article ID</th>\n"
+      ."<th scope=\"col\">Title</th>\n"
+      ."<th scope=\"col\">Author</th>\n"
+      ."<th scope=\"col\">Method</th>\n"
+      ."<th scope=\"col\">Result</th>\n"
+      ."<th scope=\"col\">DOI</th>\n"
+      ."<th scope=\"col\">Year</th>\n"
+      ."</tr>\n";
+
+      while ($row = mysqli_fetch_assoc($sqlResult))
+      {
+        echo "<tr>";
+        echo "<td>",$row["article_id"],"</td>";
+        echo "<td>",$row["title"],"</td>";
+        echo "<td>",$row["author"],"</td>";
+        echo "<td>",$row["method"],"</td>";
+        echo "<td>",$row["result"],"</td>";
+        echo "<td>",$row["doi"],"</td>";
+        echo "<td>",$row["year"],"</td>";
+        echo "</tr>";
+      }
+      echo "</table>";
+      mysqli_free_result($sqlResult);
   }
 
+}
 }
 class InputValues
 {
@@ -52,8 +107,7 @@ class InputValues
   {
 	if(isset($_POST["year"])){
 		$this->yearRange = $_POST["year"];
-		//echo $this->yearRange;
-		//echo "<br> Start Date" . $this->startDate; //gets the radio button value
+
 		$this->setStartDate();
 			if ($this->yearRange == "lastFive" ){
 				$this->yearResult = $this->startDate - 5;
@@ -65,7 +119,6 @@ class InputValues
 				$this->yearResult =  $this->startDate - 20;
 			}
 		}
-		//echo $this->yearResult;
   }
 	public function getEndDate()
   {
@@ -77,11 +130,10 @@ class InputValues
 		{
 			$this->sortSetting = $_POST["sort"];
 
-      if($this->sortSetting = 'description')
+      /*if($this->sortSetting = 'description')
       {
         $this->sortSetting = 'method';
-      }
-      echo "<br>" . $this->sortSetting . "<br>";
+      }*/
 
 		}else{
 			$this->sortSetting = "title";
@@ -89,38 +141,8 @@ class InputValues
   }
 	public function getSort()
   {
-    echo "<br>" . $this->sortSetting . "<br>";
 		return $this->sortSetting;
   }
-}
-class SQLSend
-{
-	private $connection;
-	private $search;
-	public $startDate;
-	public $yearRange;
-	private $yearResult;
-	private $sortSetting;
-	private $sqlString;
-	public function __construct($search, $startDate, $yearRange, $yearResult, $sortSetting)
-  	{
-		$this->connection = new DatabaseConnection();
-		$this->$search = $search;
-		$this->$startDate = $startDate;
-		$this->$yearRange = $yearRange;
-		$this->$yearResult = $yearResult;
-		$this->$sortSetting = $sortSetting;
-	}
-	public function QueryDB()
-	{
-		/*$this->sqlString = "SELECT * FROM `article`
-							WHERE title like '%$search%' OR author like '%$search%' OR method like '%$search%'
-							AND year BETWEEN $yearResult and $startDate
-							ORDER BY $sortSetting";*/
-
-		//return mysqli_query($connection, $sqlString);
-		return $this->sqlString;
-	}
 }
 
 ?>
@@ -141,11 +163,14 @@ class SQLSend
 		$db = new DatabaseConnection();
 		$connection= $db->dbConnect();
 
-
-		if(!$connection){
+		if(!$connection)
+    {
 			echo "connection failure";
-		}else{
-			if(isset($_POST["enter"])){
+		}
+    else
+    {
+			if(isset($_POST["enter"]))
+      {
 				$values = new InputValues();
 				$values->setSearch();
 				$search = $values->getSearch();
@@ -156,53 +181,12 @@ class SQLSend
 				$values ->setSort();
 				$sortSetting = $values -> getSort();
 
-        echo "<br>" . $sortSetting . "<br>";
+				$sqlResult = $db->selectData($connection, $search,$yearResult,$startDate,$sortSetting);
+        $db->outputData($sqlResult);
 
-				$sqlString = "SELECT * FROM `article`
-							WHERE (title like '%$search%' OR author like '%$search%' OR method like '%$search%')
-							AND year BETWEEN $yearResult and $startDate
-							ORDER BY $sortSetting";
-
-				$sqlResult = mysqli_query($connection, $sqlString);
-				echo ($sqlString);
-
-				$sqlResult = mysqli_query($connection, $sqlString);
-
-				/*$sqlSend = new SQLSend($search, $startDate, $endDate, $sortSetting);
-				$sqlStr =$sqlSend->QueryDB();
-				$sqlResult = mysqli_query($connection, $sqlStr);*/
-
-				if(!$sqlResult){
-					echo "<p>Something is wrong with ",	$sqlString , "</p> <br>";
-					//echo "<p>Year Result ",	$yearResult, "</p> <br>";
-				}else{
-					echo "<table class='table table-dark'>";
-					echo "<tr>\n"
-					."<th scope=\"col\">Article ID</th>\n"
-					."<th scope=\"col\">Title</th>\n"
-					."<th scope=\"col\">Author</th>\n"
-					."<th scope=\"col\">Method</th>\n"
-					."<th scope=\"col\">Result</th>\n"
-					."<th scope=\"col\">DOI</th>\n"
-					."<th scope=\"col\">Year</th>\n"
-					."</tr>\n";
-
-					while ($row = mysqli_fetch_assoc($sqlResult)){
-						echo "<tr>";
-						echo "<td>",$row["article_id"],"</td>";
-						echo "<td>",$row["title"],"</td>";
-						echo "<td>",$row["author"],"</td>";
-						echo "<td>",$row["method"],"</td>";
-						echo "<td>",$row["result"],"</td>";
-						echo "<td>",$row["doi"],"</td>";
-						echo "<td>",$row["year"],"</td>";
-						echo "</tr>";
-					}
-					echo "</table>";
-					mysqli_free_result($sqlResult);
 				}
 			}
-		}
+
 	?>
 <p><a class="btn btn-primary btn-lg btn-block" href="index.html" role="button">Search an another Article</a></p>
 </body>
